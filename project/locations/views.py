30dc2cb8df3a99adac958121685b3826dc0f7dbc
unknown <sorin_1314@yahoo.com>
2024-10-08 21:31:56 +0300

    project Django

---
+++ project/locations/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, UpdateView
+
+from locations.models import Location
+
+
+# Create your views here.
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import ListView
+
+from locations.models import Location
+
+
+class CreateLocationView(CreateView):
+    model = Location
+    template_name = 'forms.html'
+    fields = ['city', 'country']
+
+    def get_success_url(self):
+        return reverse('locations:lista_locatii')
+
+class LocationView(ListView):
+    model = Location
+    template_name = 'locations/locations_index.html'
+
+
+class UpdateLocationView(UpdateView):
+    model = Location
+    fields = ['city', 'country']
+    template_name = 'forms.html'
+
+    def get_success_url(self):
+        return reverse('locations:lista_locatii')
+
+
+def deactivate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=False)
+    return redirect('locations:lista_locatii')
+
+def activate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=True)
+    return redirect('locations:lista_locatii')
